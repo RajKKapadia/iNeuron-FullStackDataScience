67dfc9f822af75c08b3b85ae946e123797167d82
RajKKapadia <raajforyou@gmail>
2022-02-21 18:59:12 +0530

    Python Programming Basic Assignment, assignment one

---
+++ Python Programming Basic Assignment/Assignment_One/three.py
@@ -0,0 +1,6 @@
+h = int(input('Enter height of the triangle: '))
+b = int(input('Enter base of the triangle: '))
+
+a = 0.5 * h * b
+
+print(f'The are of a triangle with height {h} and base {b} is {round(a, 2)}.')
---
+++ Python Programming Basic Assignment/Assignment_One/two.py
@@ -0,0 +1,13 @@
+a = 10
+b = 7
+
+c = a + b
+
+print(f'The sum of {a} and {b} is {c}.')
+
+a = 10
+b = 7
+
+c = a / b
+
+print(f'{a} divided by {b} is {round(c, 2)}.')
